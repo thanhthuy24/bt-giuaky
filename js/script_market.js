cd1a1038b0065e6fa98a322a3b273406f43f2b5c
master-piece <hothanhthuy11.2003@gmail.com>
2022-08-04 10:32:05 +0700

    BT

---
+++ js/script_market.js
@@ -0,0 +1,8 @@
+/*khi nhan vao button copy*/
+function init(){
+    let button = document.querySelectorAll(".but input");
+    for (let i = 0; i < button.length; i++)
+        button[i].addEventListener("click", function(){
+        let prefix = this.getAttribute("rel");
+  })
+}
